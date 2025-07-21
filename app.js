ac86d66ff84c1c0aef0c077859ad62f1b386b77f
Yusif-Abbaszade <yusif.abbaszade.043@gmail.com>
2025-07-21 20:57:30 +0400

    authentication

--- app.js
+++ app.js
@@ -7,13 +7,21 @@ import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 
 app.use('/api/auth', authRouter);
 app.use('/api/users', userRouter);
 app.use('/api/subscriptions', subscriptionRouter);
+
+app.use(errorMiddleware);
 
 app.get('/', (req, res) => {
     res.send('salam')
