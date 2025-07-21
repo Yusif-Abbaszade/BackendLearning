79c599b833c84f1b2b89c5d1656844d10a16ab51
Yusif-Abbaszade <yusif.abbaszade.043@gmail.com>
2025-07-21 22:01:36 +0400

    authorization

---
+++ controllers/user.controller.js
@@ -0,0 +1,34 @@
+import User from "../models/user.model.js";
+
+export const getUsers = async (req, res, next) => {
+    try {
+        const users = await User.find();
+
+        res.status(200).json({ success: true, data: users });
+    }
+    catch (error) {
+        next(error);
+    }
+}
+
+export const getUser = async (req, res, next) => {
+    try {
+        if(req.user.id !== req.params.id){
+            const error = new Error('Unauthorized');
+            error.statusCode = 401;
+            throw error;
+        }
+        const user = await User.findById(req.params.id).select('-password');
+
+        if(!user){
+            const error = new Error('User not found');
+            error.statusCode = 404;
+            throw error;
+        }
+
+        res.status(200).json({ success: true, data: user });
+    }
+    catch (error) {
+        next(error);
+    }
+}
